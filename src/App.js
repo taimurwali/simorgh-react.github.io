097b34b657de8110abc7a62b09bc9c42edd92479
taimurwali <taimur.wali@spsnet.com>
2020-07-30 12:21:37 +0500

    props test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import HorizontalScroll from 'react-scroll-horizontal'
+import HorizontalScroll from 'react-scroll-horizontal';
+
 import './App.css';
+import Test from './Main';
+
+
+
 
 function App() {
   return (
-    <div className="App-div">
-      <HorizontalScroll>
-          <div  className="sec-1">asda</div>
-          <div   className="sec-2"> 123123</div>
-          <div   className="sec-3"> @!#!@#</div>
-      </HorizontalScroll>
+    <div>
+      <Test firstName="taimur" lastName="wali" /> 
     </div>
+    // <div className="App-div">
+    //   <HorizontalScroll reverseScroll = {true}>
+    //       <div  className="sec-1">asda</div>
+    //       <div   className="sec-2"> 123123</div>
+    //       <div   className="sec-3"> @!#!@#</div>
+    //   </HorizontalScroll>
+    //   </div>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Test(props) {
+    return (
+    <h1>Hello World! {props.firstName} and {props.lastName}</h1>
+    );
+  }
+
+export default Test;
