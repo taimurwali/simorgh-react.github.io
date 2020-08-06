2cdfa42cfd48b71102353eead26c0540a9150f04
taimurwali <taimur.wali@spsnet.com>
2020-08-06 13:47:20 +0500

    videos

--- src/life.js
+++ src/life.js
@@ -2,47 +2,62 @@ import React from 'react';
 import './hero.css';
 import brush from '../src/b2.png'; // with import
 import brush2 from '../src/b1.png'; // with import
-
+import { Player, ControlBar, BigPlayButton   } from 'video-react';
+import './video-react.css';
 
 
 function Life() {
 
-    return (
+  return (
     <div className='sec-3'>
-        <h3>Life at Simorgh</h3>
+      <h3>Life at Simorgh</h3>
 
-        <img className="brush" src={brush} />
-        <img className="brush-2" src={brush2} />
+      <img className="brush" src={brush} />
+      <img className="brush-2" src={brush2} />
+        
+        <div className="inline v1">
+          <Player
+      controls= {false}
+      autoPlay ={false}
+      src="https://videos.ctfassets.net/y920cgi13iu8/1abytSbPZ8kUs9h5pu50M6/d83d8fd5b592b8a89b74b268a6f19bf7/CarlosVelazquez_BeSelfless_Part1_MASTER_H264.mp4"
+    >
+            <BigPlayButton position="center" />
+      <ControlBar autoHide={false} className="my-class" />
+    </Player>
+        </div>
 
-
-
-
-        <div id="process-wrap">
-<div id="process" class="horizontal-container">
-  <section class="horz">
-      <div class="wrap">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5321.96 736.46">
-            <text class="step step-01" transform="translate(90 192.72)">Discovery</text>
-            <text class="step step-02" transform="translate(692.33 340.72)">Research</text>
-            <text class="step step-03" transform="translate(1330.24 295.72)">Concept</text>
-            <text class="step step-04" transform="translate(1952.1 396.72)">Client Approval</text>
-            <text class="step step-05" transform="translate(2630.96 42.72)">Design</text>
-            <text class="step step-06" transform="translate(3042.1 245.72)">Client Approval</text>
-            <text class="step step-07" transform="translate(3883.88 190.72)">Production</text>
-            <text class="step step-08" transform="translate(4480.1 372.72)">Client Approval</text>
-            <text class="step step-09" transform="translate(5105.81 192.72)">Launch</text>
-
-            <path class="time" stroke-linecap="round" stroke-width="24px" stroke-miterlimit="10" d="M655,527.31s322.07-25.41,533.19,93.83c71.79,40.55,172.58,86.68,266.25,112.56,18,5,103.06,30,210.7,21.67,151.82-11.73,329.71-90.74,383.13-117.12,144-71.08,340.79-73.54,461.33-75.1,85.24-1.1,267.8-.65,477.6-112.72S3531.93,302.31,3773,492.78s521.92,227.4,799.49,92.52,520-63.85,520-63.85" transform="translate(-200.6 -342.53)"/>
-
-
-          </svg>
-      </div>
-  </section>
-  </div></div>
-
-
-
-    </div>
+        <div class="quote-block" ><div class="count"><span>5</span>/5</div><span class="headline">Be selfless</span><div class="quote-icon"><div class="text">“</div></div><blockquote class="underline-child quote quote--default"><p class="quote__text ">We recruit and promote people who want to win as <i>part of a great </i><u><i>company</i></u>.</p></blockquote></div>
+        <div className="inline v2">
+          <Player
+      controls= {false}
+      autoPlay ={false}
+      src="http://videos.ctfassets.net/y920cgi13iu8/6GF5ybJ2vTO6GTyryLyNoX/beb561bf3c7890e6a87833dccb217bef/AlexBerk_BeSelfless_MASTER_H264.mp4"
+    >
+            <BigPlayButton position="center" />
+      <ControlBar autoHide={false} className="my-class" />
+    </Player>
+        </div>
+        <div className="inline v3">
+          <Player
+      controls= {false}
+      autoPlay ={false}
+      src="https://videos.ctfassets.net/y920cgi13iu8/xjv03EjGQPZ4qKz7prk7G/a4f95dccedbdc0f730f96747f4da576b/AlmeidaDye_BeSelfless_MASTER_H264.mp4"
+    >
+            <BigPlayButton position="center" />
+      <ControlBar autoHide={false} className="my-class" />
+    </Player>
+        </div>
+        <div className="inline v4">
+          <Player
+      controls= {false}
+      autoPlay ={false}
+      src="https://videos.ctfassets.net/y920cgi13iu8/1abytSbPZ8kUs9h5pu50M6/d83d8fd5b592b8a89b74b268a6f19bf7/CarlosVelazquez_BeSelfless_Part1_MASTER_H264.mp4"
+    >
+            <BigPlayButton position="center" />
+      <ControlBar autoHide={false} className="my-class" />
+    </Player>
+        </div>
+            </div>
     );
 }
 
